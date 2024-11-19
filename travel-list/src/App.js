45f58373836742bd2416e94cf4d2207d9542ebf9
DanielMevs <dannymevs@hotmail.com>
2024-11-18 19:26:35 -0500

    started building the form and handling submissions for travel list app

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 1, description: "Charger", quantity: 2, packed: false },
-  { id: 1, description: "Tooth brush", quantity: 2, packed: true },
+  { id: 3, description: "Charger", quantity: 2, packed: false },
+  { id: 4, description: "Tooth brush", quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -18,10 +20,35 @@ export default function App() {
     return <h1>🏝️ Far Away 💼</h1>;
   }
   function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    function handleSubmit(e) {
+      e.preventDefault();
+      if (!description) return;
+      const newItem = { description, quantity, packed: false, id: Date.now() };
+      console.log(newItem);
+    }
     return (
-      <div className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you need for your 😍 trip?</h3>
-      </div>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
     );
   }
   function PackingList() {
@@ -29,7 +56,7 @@ export default function App() {
       <div className="list">
         <ul>
           {initialItems.map((item) => (
-            <Item item={item} />
+            <Item item={item} key={item.id} />
           ))}
         </ul>
       </div>
