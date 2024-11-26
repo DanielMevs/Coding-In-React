f8d749b8b2e6cd9ccb0a7f70ba9cbbc8c79bfb0c
DanielMevs <dannymevs@hotmail.com>
2024-11-25 19:30:45 -0500

    reorganized travel-list to put components in their own file and modified steps to use children props

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
+      <Steps />
     </div>
   );
 }
@@ -47,36 +47,21 @@ function Steps() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
-            <Button
-              textColor="#fff"
-              bgColor="#7950f2"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
-              text="Previous"
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button
-              textColor="#fff"
-              bgColor="#7950f2"
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
-              text="Next"
             >
-              Next <span>👉</span>
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
